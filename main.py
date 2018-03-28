858dbb871bda746a1afb4c63c4b949b8bfb469df
Blake Molina <blakeamolina@gmail.com>
2018-03-28 10:52:54 -0700

    Use argparse for handling command line args

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
 import sys
+import argparse
 from PIL import Image
 
-
-'''
-
-Implementation:
-Encode & Decode functions
-
-Encode function
-input params: jpeg image, string of text
-output params: png image with embedded text (with size of the text on bottom right 11 pixels)
-
-details:
-1). Validate params
-2). Convert text and text length to binary
-3). Use the bottom right 11 pixels to place text length
-4). Use remaining pixels to store text
-
-Decode function
-input params: png image
-output params: A string containing the embedded text in the png image
-
-details:
-1). Validate params
-2). Extract text length from image - convert from binary to string
-3). Extract text from image - convert from binary to string
-
-'''
 
 # Number of pixels to store the length of the input text.
 TEXT_LEN_PIXELS = 11
 
-# Enbeds text in an image
+# Mask to extract least significant bit of an 8-bit binary number
+MASK = 0b0000001
+
 def encode(file_path, text):
+    """
+    Encodes text in an image. More specifically, the text is embedded in the least significant bit
+    of each RGB value within each pixel of an image. The bottom right 11 pixels of the image are
+    reserved for storing the length of the text.
+
+    :param file_path: The relative path to the image.
+    :param text: The text to encode into the image.
+    :return: An image object, containing the embedded text.
+    """
     if not file_path.endswith(".jpeg") and not file_path.endswith(".jpg"):
         raise InvalidImageTypeException("Error: image to encode must be .jpg format")
     if not text.strip():
@@ -43,7 +29,7 @@ def encode(file_path, text):
     pixmap_before = before.load()
 
     '''
-    Verify that the input text will fit into the image. We subtract the numer of pixels needed to 
+    Verify that the input text will fit into the image. We subtract the number of pixels needed to 
     store the input text size, and then multiply the number of pixels by 3 because there are 3 RGB 
     values in each pixel, and each bit of input text will be placed in the LSB of each RGB value.
     '''
@@ -64,17 +50,14 @@ def encode(file_path, text):
 
 # Function to encode 'len bits' within the first 11 pixels starting from the bottom right of an image.
 def encode_text(before, pixmap_before, after, pixmap_after, text_bits):
+    # TODO: merge this and the 'encode' function together.
 
     # Get length (in bits) of the input text represented in bits. Then place on the bottom right 11 pixels.
     text_bits_len = len(text_bits)
     len_bits = bin(text_bits_len)[2:]
     index = len(len_bits) - 1
     for x in range(TEXT_LEN_PIXELS):
-        # Get pixel values and convert them to their binary representations.
-        r, g, b = pixmap_before[before.size[0] - x - 1, before.size[1] - 1]
-        r_bin = int(bin(r), 2)
-        g_bin = int(bin(g), 2)
-        b_bin = int(bin(b), 2)
+        r_bin, g_bin, b_bin = get_pixels_bin(pixmap_before, before.size[0] - x - 1, before.size[1] - 1)
 
         if index - 2 >= 0:
             b_bin = set_bit(r_bin, 0) if len_bits[index] == 1 else clear_bit(r_bin, 0)
@@ -97,11 +80,7 @@ def encode_text(before, pixmap_before, after, pixmap_after, text_bits):
             if y == before.size[1] -1 and x == before.size[0] - TEXT_LEN_PIXELS - 1:
                 break
 
-            # Get pixel values and convert them to their binary representations
-            r, g, b = pixmap_before[x, y]
-            r_bin = int(bin(r), 2)
-            g_bin = int(bin(g), 2)
-            b_bin = int(bin(b), 2)
+            r_bin, g_bin, b_bin = get_pixels_bin(pixmap_before, x, y)
 
             if index + 3 <= text_bits_len:
                 r_bin = set_bit(r_bin, 0) if text_bits[index] == 1 else clear_bit(r_bin, 0)
@@ -114,34 +93,74 @@ def encode_text(before, pixmap_before, after, pixmap_after, text_bits):
     return after
 
 
-# Decodes an image containing hidden text, if any exsits.
 def decode(file_path):
+    """
+    Decodes an image containing hidden text, if any hidden text exists.
+
+    :param file_path: The relative path to the image.
+    :return: The text embedded in the image. If no text exists, then an empty
+    string will be returned.
+    """
     if not file_path.endswith(".png"):
-        raise InvalidImageTypeException("Error: image to decode must be .png format")
+        raise InvalidImageTypeException("Error: image to decode must be .png format.")
+
+    img = Image.open(file_path)
+    pixmap = img.load()
+
+    length_in_bits = ''
+    for x in range(TEXT_LEN_PIXELS):
+        r_bin, g_bin, b_bin = get_pixels_bin(pixmap, img.size[0] - x - 1, img.size[1] - 1)
+        length_in_bits += str(b_bin & MASK)
+        length_in_bits += str(g_bin & MASK)
+        length_in_bits += str(r_bin & MASK)
+
+    return int(length_in_bits)
 
 
-# Function for extracting command line arguments.
-def getopts(argv):
-    opts = {}
-    while argv:
-        if argv[0][0] == '-':
-            opts[argv[0]] = argv[1]
-        argv = argv[1:]
-    return opts
+def get_pixels_bin(pixmap, x, y):
+    """ 
+    Returns 3 RGB values from a pixel map at the specified x and y coordinates. Note that these values
+    are returned as binary numbers.
+    
+    :param pixmap: A PIL image access object.
+    :param x: The x coordinate of the pixel.
+    :param y: The y coordinate of the pixel.
+    :return: Three RGB values represented as binary numbers.
+    """""
+
+    r, g, b = pixmap[x, y]
+    r_bin = int(bin(r), 2)
+    g_bin = int(bin(g), 2)
+    b_bin = int(bin(b), 2)
+    return r_bin, g_bin, b_bin
 
 
-# Function to set a single bit in 'value' to 1.
 def set_bit(value, bit):
+    """
+    Sets a single bit in 'value' to 1.
+    :param value: The number.
+    :param bit: The bit to set to 1.
+    :return: The value with one of its bits set to 1.
+    """
     return value | (1 << bit)
 
 
-# Function to set a single bit in 'value' to 0.
 def clear_bit(value, bit):
+    """
+    Sets a single bit in 'value' to 0.
+    :param value: The number.
+    :param bit: The bit to set to 0.
+    :return: The value with one of its bits set to 0.
+    """
     return value & ~(1 << bit)
 
 
-# Function to convert a string into an array of bytes. Note that the input string must be in UTF-8 format.
 def tobits(s):
+    """
+    Converts a string into a list of bits. Note that the input string must be in UTF-8 format.
+    :param s: The string to convert.
+    :return: A list of bits.
+    """
     result = []
     for c in s:
         bits = bin(ord(c))[2:]
@@ -150,8 +169,12 @@ def tobits(s):
     return result
 
 
-# Function to convert from an array of bits to a String
 def frombits(bits):
+    """
+    Converts an array of bits to an integer string.
+    :param bits: The list of bits to convert.
+    :return: A string with the integer value of the bits.
+    """
     chars = []
     for b in range(int(len(bits) / 8)):
         byte = bits[b*8:(b+1)*8]
@@ -171,21 +194,18 @@ class IllegalArgumentError(ValueError):
         super().__init__(message)
 
 
+# Driver function
 if __name__ == '__main__':
-    from sys import argv
-    args = getopts(argv)
-    usage_msg = "Usage: ./file.py [-p <path to image>] [-e <text to insert>] [-d]"
+    parser = argparse.ArgumentParser(usage='./PySteg [path] [-e string] [-d] [-h]')
+    parser.add_argument('path', help='The relative path of the image to use')
+    parser.add_argument('-e', '--encode', help='Text to encode in an image')
+    parser.add_argument('-d', '--decode', help='Decode from an image', action='store_true')
+    args = parser.parse_args()
 
-    if '-p' not in args:
-        raise IllegalArgumentError(usage_msg)
-    if '-e' not in args and '-d' not in args:
-        raise IllegalArgumentError(usage_msg)
-
-    if '-e' in args:
-        result = encode(args.get('-p'), args.get('-e'))
+    if args.encode:
+        result = encode(args.path, args.encode)
         result.show()
         result.save('output.png')
-
-    elif '-d' in args:
-        message = decode(args.get('-d'))
-        print(message)
+    elif args.decode:
+        length = decode(args.path)
+        print(length)
